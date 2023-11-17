d267adfdb450a43b0e6e47d3cdc56ce2935c1acd
Igor Bazei <igorbazei.ilb@gmail.com>
2023-11-17 15:34:22 -0300

    SignIn

--- Programação II/frontend/src/index.js
+++ Programação II/frontend/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './components/App';
+import SignIn from './components/SignIn';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <SignIn />
   </React.StrictMode>
 );
 
